4a65248b746983446c84c48c2e16d73df86b89de
The3gamer <vcbart001@hotmail.com>
2013-02-21 17:05:03 +0100

    Create ServerCoreRun.java

---
+++ ServerCoreRun.java
@@ -0,0 +1,40 @@
+minecraft.server.minegaming.the3gamer.serverstartup;
+
+public static minegaming.the3gamer.startup
+public static minegaming.the3gamer.startup.hello
+public static minegaming.the3gamer.startup.hello.console 
+public static minegaming.the3gamer.startup.hello.console.output
+
+System.out.printIn("Hello Owner please set this server up I am the console i can speak") 
+
+public static minecraft.net.src.server
+public static minecraft.net.src.server.connect
+public static minecraft.net.src.server.connect.hamachi
+
+public static java.util.random
+
+public static minegaming.the3gamer.group.Minegaming_Creator.make
+
+public static minegaming.the3gamer.group.Minegaming_Creator.assign.user.The_3_gamers
+
+public static minecraft.net.src.server.group.Minegaming_Creator.assign.user.The_3_gamers 
+public static minecraft.net.src.server.group.Minegaming_Creator.assign.user.The_3_gamers.config
+public static minecraft.net.src.server.group.Minegaming_Creator.assign.user.The_3_gamers.config.true
+
+
+public class ServerConnectSetup
+
+       {
+      {
+     {
+    }
+   }
+  }
+ }
+}
+
+
+
+
+
+
